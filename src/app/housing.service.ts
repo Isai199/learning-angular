46d25046666717b75699e5604bf37c5f9a3416b4
Isai199 <h_isai_gomez@hotmail.com>
2024-06-04 21:43:55 -0600

    UPDATE(http connection): hice una conxion de manera local, solo tuve un detalle con la referencia de la propiedad para las fotos

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -5,117 +5,19 @@ import { Housinglocation } from './housinglocation';
   providedIn: 'root'
 })
 export class HousingService {
-  readonly baseUrl = 'https://primefaces.org/cdn/primeng/images/galleria';
+  // TODO: hacer que el servidor local tome como referencia la url para las imagenes
+  // readonly baseUrl = 'https://primefaces.org/cdn/primeng/images/galleria';
 
-  housingLocationList: Housinglocation[] = [
-    {
-      id: 0,
-      name: 'Acme Fresh Start housing',
-      city: 'Chicago',
-      state: 'IL',
-      photo: `${this.baseUrl}/galleria1s.jpg`,
-      availableUnits: 4,
-      wifi: true,
-      laundry: true,
-    },
-    {
-      id: 1,
-      name: 'A113 Transitional Housing',
-      city: 'Santa Monica',
-      state: 'CA',
-      photo: `${this.baseUrl}/galleria2s.jpg`,
-      availableUnits: 0,
-      wifi: false,
-      laundry: true,
-    },
-    {
-      id: 2,
-      name: 'Warm Beds Housing Support',
-      city: 'Juneau',
-      state: 'AK',
-      photo: `${this.baseUrl}/galleria3s.jpg`,
-      availableUnits: 1,
-      wifi: false,
-      laundry: false,
-    },
-    {
-      id: 3,
-      name: 'Homesteady Housing',
-      city: 'Chicago',
-      state: 'IL',
-      photo: `${this.baseUrl}/galleria4s.jpg`,
-      availableUnits: 1,
-      wifi: true,
-      laundry: false,
-    },
-    {
-      id: 4,
-      name: 'Happy Homes Group',
-      city: 'Gary',
-      state: 'IN',
-      photo: `${this.baseUrl}/galleria5s.jpg`,
-      availableUnits: 1,
-      wifi: true,
-      laundry: false,
-    },
-    {
-      id: 5,
-      name: 'Hopeful Apartment Group',
-      city: 'Oakland',
-      state: 'CA',
-      photo: `${this.baseUrl}/galleria6s.jpg`,
-      availableUnits: 2,
-      wifi: true,
-      laundry: true,
-    },
-    {
-      id: 6,
-      name: 'Seriously Safe Towns',
-      city: 'Oakland',
-      state: 'CA',
-      photo: `${this.baseUrl}/galleria7s.jpg`,
-      availableUnits: 5,
-      wifi: true,
-      laundry: true,
-    },
-    {
-      id: 7,
-      name: 'Hopeful Housing Solutions',
-      city: 'Oakland',
-      state: 'CA',
-      photo: `${this.baseUrl}/galleria8s.jpg`,
-      availableUnits: 2,
-      wifi: true,
-      laundry: true,
-    },
-    {
-      id: 8,
-      name: 'Seriously Safe Towns',
-      city: 'Oakland',
-      state: 'CA',
-      photo: `${this.baseUrl}/galleria9s.jpg`,
-      availableUnits: 10,
-      wifi: false,
-      laundry: false,
-    },
-    {
-      id: 9,
-      name: 'Capital Safe Towns',
-      city: 'Portland',
-      state: 'OR',
-      photo: `${this.baseUrl}/galleria10s.jpg`,
-      availableUnits: 6,
-      wifi: true,
-      laundry: true,
-    },
-  ];
+  url = 'http://localhost:3000/locations';
 
-  getAllHousingLocations(): Housinglocation[] {
-    return this.housingLocationList;
+  async getAllHousingLocations(): Promise<Housinglocation[]> {
+    const data = await fetch(this.url);
+    return (await data.json()) ?? [];
   }
 
-  getHousingLocationById(id: number): Housinglocation | undefined {
-    return this.housingLocationList.find((Housinglocation) => Housinglocation.id === id);
+  async getHousingLocationById(id: number): Promise<Housinglocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return (await data.json()) ?? {};
   }
 
   submitApplication(firstName: string, lastName: string, email: string) {
